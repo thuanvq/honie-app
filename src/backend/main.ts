755e3d33c333de361fe3442f76363d21f3ff4f1a
unknown <thuan@meta-technology.com.vn>
2024-06-04 17:25:49 +0700

    finish adsense screen

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -2,12 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(AppModule);
-  nestApp.enableCors({
-    origin: '*', // Allow requests from file protocol
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept',
-  });
-  await nestApp.listen(3000);
+  const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors();
+
+  await app.listen(3000);
 }
 bootstrap();
