b7a48e33fc47458c1a63c6146c321084e6992f43
unknown <thuan@meta-technology.com.vn>
2024-06-05 16:24:30 +0700

    finish common

--- src/constants.ts
+++ src/constants.ts
@@ -12,9 +12,17 @@ export const COUNTRY_CODE: Record<string, string> = {
 export type LIST_RESPONSE = { title: string; headers: any[]; data: any[]; filters: any[]; totalRecords: number };
 
 export const SORT_FIELDS = {
+  today: 'todayReport.estimatedEarnings',
   rpm: 'todayReport.pageRPM',
   views: 'todayReport.pageViews',
   impressions: 'todayReport.impressions',
   updated: 'todayReport.updatedAt',
   clicks: 'todayReport.clicks',
+  yesterday: 'yesterdayReport.estimatedEarnings',
+  month: 'monthReport.estimatedEarnings',
+  limit: 'information.limit',
+  country: 'information.country',
+  utc: 'information.utc',
+  owner: 'information.owner',
+  currency: 'information.currency',
 };
